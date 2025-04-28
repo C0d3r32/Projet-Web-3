1a207ea05d8a4fb53649e322b2599c223d6f9be8
C0d3r32 <164863187+C0d3r32@users.noreply.github.com>
2025-04-28 14:50:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,78 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Informations sur la BDD et le serveur qui la contient
 $db_name = "projet" ;
  $db_host = "127.0.0.1" ; 
  $db_port = "3306" ;
 
-// Informations d'authentification de votre script PHP :
 $db_user = "root" ; 
 $db_pwd = "" ;
 
-// Connexion à la BDD
 try{
-    // Agrégation des informations de connexion dans une chaine DSN (Data Source Name)
     $dsn = 'mysql:dbname=' . $db_name . ';host='. $db_host. ';port=' . $db_port;
-
-    // Connexion et récupération de l'objet connecté
     $pdo = new PDO($dsn, $db_user, $db_pwd);
 }
 
-// Récupération d'une éventuelle erreur
 catch (\Exception $ex){ ?>
-    <!-- Affichage des informations liées à l'erreur-->
     <div style="color: red">
         <b>!!! ERREUR DE CONNEXION !!!</b><br>
         Code : <?= $ex->getCode() ?><br>
         Message : <?= $ex->getMessage() ?>
     </div><?php
-    // Arrêt de l'exécution du script PHP
-    die("-> Exécution stoppée <-") ;
+    //die("-> Exécution stoppée <-") ;
 }
 
-// Poursuite de l'exécution du script ?>
-<div style="color: green">Connecté à <b><?= $dsn ?></b></div> <?php
+
+require "modules/classes/Autoloader.php" ;
+Autoloader::register();
+
+$logger = new logger\Logger() ;
+
+if (isset($_POST['username']) and isset($_POST['password'])){
+    $response = $logger->log(trim($_POST['username']), $_POST['password']) ;
+    if ($response['granted']){
+        $nick = $response['nick'] ;
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CinéList</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="modules/css/style.css">
+
+</head>
+<body>
+
+<?php include "modules/pages/elements/header.php" ?>
+
+<div id="main-content">
+    <?php
+        if (!isset($response)) :
+            $logger->generateLoginForm("");
+        elseif (!$response['granted']) :
+            echo "<div class='magic-card' id='error'>" .$response['error']."</div>" ;
+            $logger->generateLoginForm("");
+        else :
+            include "modules/pages/elements/admin.php" ;
+        endif;
+    ?>
+
+</div>
+
+<?php include "modules/pages/elements/footer.php" ?>
+
+</body>
+</html>
